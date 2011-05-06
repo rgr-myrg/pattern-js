c7feff130fc68d04fac9170a617a6739bf0e4a12
Roger Myrg <rpmyrg@gmail.com>
2011-05-06 19:17:39 -0400

    Added file headers

--- src/observable.js
+++ src/observable.js
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2011 Roger Myrg http://devshop.me/
  * Released under the MIT license:
- * https://github.com/rgr-myrg/JavaScript-Observer-Plus/raw/master/MIT-LICENSE
+ * https://github.com/rgr-myrg/DevShop-JS/raw/master/MIT-LICENSE
  */
 (function(){
 	DevShop.Me({

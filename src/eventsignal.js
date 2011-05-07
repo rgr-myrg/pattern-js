7278e0cc0b396ffdf5ea6b7324b8e2c2dd5eca79
Roger Myrg <rpmyrg@gmail.com>
2011-05-06 22:12:12 -0400

    Added EventSignal

---
+++ src/eventsignal.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2011 Roger Myrg http://devshop.me/
+ * Released under the MIT license:
+ * https://github.com/rgr-myrg/DevShop-JS/raw/master/MIT-LICENSE
+ */
+(function(){
+	DevShop.Me({
+		EventSignal:function(obj){
+			var listeners=[];
+			this.addListener=function(listener){
+				if(typeof listener==='function')
+					listeners.push(listener);
+			};
+			this.removeListener=function(listener){
+				var size=listeners.length;
+				for(var x=0;x<size;x++)
+					if(listeners[x]===listener)
+						listeners.splice(x);
+			};
+			this.dispatch=function(){
+				var size=listeners.length;
+				for(var x=0;x<size;x++)
+					try{listeners[x].apply(this,arguments);}catch(e){}
+			};
+		}
+	});
+})();

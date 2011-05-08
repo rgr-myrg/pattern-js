b07d62f806bb7793efc7a4fee170673ab9f940bd
Roger Myrg <rpmyrg@gmail.com>
2011-05-08 18:42:03 -0400

    Added 'full' build

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 #Build Script for http://DevShop.Me
 
-VERSION="0.1"
-ARTIFACT="devshop.js"
-JS_FILES="--js src/core.js --js src/singletonfactory.js --js src/observable.js --js src/observer.js --js src/eventsignal.js --js src/mvc.js"
-JS_BUILD="build/$ARTIFACT"
 JS_TEMP="build/tmp.js"
 EXAMPLE="examples/devshop.js"
 LICENSE="LICENSE"
-PACKAGES=(eventsignal mvc observer singletonfactory);
+PACKAGES=(eventsignal mvc observer singletonfactory full);
 
 echo -n "Compiling..."
 
@@ -34,17 +30,18 @@ do :
 		files="--js src/core.js --js src/singletonfactory.js"
 	;;
 	esac
+	case "$i" in
+		"full")
+		files="--js src/core.js --js src/singletonfactory.js --js src/observable.js --js src/observer.js --js src/eventsignal.js --js src/mvc.js"
+	;;
+	esac
 	artifact="build/devshop-"$i".js"
 	java -jar lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS $files --js_output_file $JS_TEMP
 	cat $LICENSE $JS_TEMP > $artifact
-	rm $temp
+	rm $JS_TEMP
 done
 
-java -jar lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS $JS_FILES --js_output_file $JS_TEMP
-cat $LICENSE $JS_TEMP > $JS_BUILD
-echo -n "Created $JS_BUILD"
-cp -p $JS_TEMP $EXAMPLE
-rm $JS_TEMP
+cp -p build/devshop-full.js examples/devshop-full.js
 
 if [ $? -ne 0 ]; then
 	exit 1
